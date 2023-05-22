677098beab710c241c3496b4275919cd85c533cd
HQ1\ahassan <abubakar.hassan@nakisa.com>
2023-05-22 16:31:00 +0500

    Solution to problem named 'Caesar Cipher'

---
+++ hackerrank solutions/src/CaesarCipher.java
@@ -0,0 +1,26 @@
+public class CaesarCipher {
+  public static String caesarCipher(String s, int k) {
+    // Write your code here
+    String response = "";
+    k = k % 26;
+    for (char c : s.toCharArray()) {
+      if (c >= 'a' && c <= 'z') {
+        if (c + k > 'z') {
+          response += (char) (k - ('z' - c) + 'a' - 1);
+        } else {
+          response += (char) (c + k);
+        }
+
+      } else if (c >= 'A' && c <= 'Z') {
+        if (c + k > 'Z') {
+          response += (char) (k - ('Z' - c) + 'A' - 1);
+        } else {
+          response += (char) (c + k);
+        }
+      } else {
+        response += c;
+      }
+    }
+    return response;
+  }
+}
