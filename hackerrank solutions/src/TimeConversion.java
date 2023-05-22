3d5e6fdd555d84d874b7bd5fc404f27f94f25bd0
HQ1\ahassan <abubakar.hassan@nakisa.com>
2023-05-22 16:34:50 +0500

    Time Conversion Solution

---
+++ hackerrank solutions/src/TimeConversion.java
@@ -0,0 +1,17 @@
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public class TimeConversion {
+  public static String timeConversion(String s) {
+    // Write your code here
+    return LocalTime.parse(
+            s,
+            DateTimeFormatter.ofPattern(
+                "hh:mm:ssa",
+                Locale.US
+            )
+        )
+        .format(DateTimeFormatter.ofPattern("HH:mm:ss"));
+  }
+}
