53f86c33eaecbd02857d2766cf7c6d18f0b027eb
HQ1\ahassan <abubakar.hassan@nakisa.com>
2023-05-22 16:36:32 +0500

    Find the Median Solution

---
+++ hackerrank solutions/src/FindTheMedian.java
@@ -0,0 +1,12 @@
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FindTheMedian {
+  public static int findMedian(List<Integer> arr) {
+    // Write your code here
+    arr = arr.stream().sorted().collect(Collectors.toList());
+    int middle = arr.size() / 2;
+    return arr.get(middle);
+  }
+
+}
