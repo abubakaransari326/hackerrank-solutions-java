0f53faca00089757f8a88805d4fd091cb8109564
abubakaransari326 <abubakaransari326@gmail.com>
2023-05-23 23:15:39 +0500

    Huffman Decoding Solution

---
+++ hackerrank solutions/src/Intermediate/HuffmanDecoding.java
@@ -0,0 +1,19 @@
+package Intermediate;
+
+
+public class HuffmanDecoding {
+  void decode(String s, Node root) {
+    Node copy = root;
+    for (char val : s.toCharArray()) {
+      if (val == '1')
+        copy = copy.right;
+      else
+        copy = copy.left;
+
+      if (copy.data != 0) {
+        System.out.print(copy.data);
+        copy = root;
+      }
+    }
+  }
+}
