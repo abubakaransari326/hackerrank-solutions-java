d501d2c63b3ca6746e69f6fca9ed47f67c94c8b7
abubakaransari326 <abubakaransari326@gmail.com>
2023-05-25 18:55:12 +0500

    String Reverse Solution

---
+++ hackerrank solutions/src/Java/JavaStringReverse.java
@@ -0,0 +1,19 @@
+package Java;
+
+import java.util.Scanner;
+
+public class JavaStringReverse {
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    String A = sc.next();
+
+    String reverse = new StringBuilder(A).reverse().toString();
+    if (A.equals(reverse))
+      System.out.println("Yes");
+    else
+      System.out.println("No");
+
+
+  }
+}
